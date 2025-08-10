c828b49c572cf4d14f3a46cc0afba3b53084ae99
monboga <monroygabriel9@gmail.com>
2025-08-10 13:18:39 -0600

    feat: se implementaron nuevas funciones que realizan la adicion de un amigo y mostrarlos por pantalla

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// declaramos un arreglo vacio de amigos
+let arregloDeAmigos = [];
+
+function agregarAmigo() {
+    // declaramos la entrada de amigos y obtenemos su valor
+    let campoTextoAmigos = document.querySelector('#amigo');
+    let amigo = campoTextoAmigos.value.trim();
+
+    if (amigo !== "") {
+        arregloDeAmigos.push(amigo);
+        campoTextoAmigos.value = "";
+        mostrarListadoAmigos();
+    }
+
+}
+
+function mostrarListadoAmigos() {
+
+    let elementoUlHTML = document.querySelector('#listaAmigos');
+    elementoUlHTML.innerHTML = '';
+
+    for (let i = 0; i < arregloDeAmigos.length; i++) {
+        let listaHTML = document.createElement('li');
+        listaHTML.textContent = arregloDeAmigos[i];
+        elementoUlHTML.appendChild(listaHTML);
+
+    }
+
+}
+
+
