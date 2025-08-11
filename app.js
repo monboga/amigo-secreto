275446f5f24f1e55b728758ba8b6dc5f7fd1c2e8
monboga <monroygabriel9@gmail.com>
2025-08-10 21:07:46 -0600

    feat: se implemento funcion para sortear y validar cuando no se ingresa algun valor en el campo de texto o arreglo vacio

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // declaramos un arreglo vacio de amigos
 let arregloDeAmigos = [];
+let amigoSecreto = 0;
 
+// funcion para obtener elemento html
+function asignarTextoHTML(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return elementoHTML
+}
+
+
+// inicio de la funcion par agregar un amigo al arreglo.
 function agregarAmigo() {
     // declaramos la entrada de amigos y obtenemos su valor
     let campoTextoAmigos = document.querySelector('#amigo');
     let amigo = campoTextoAmigos.value.trim();
 
-    if (amigo !== "") {
+    if (amigo !== '') {
         arregloDeAmigos.push(amigo);
-        campoTextoAmigos.value = "";
+        campoTextoAmigos.value = '';
         mostrarListadoAmigos();
+    } else {
+        alert('Debes ingresar texto en el campo de texto');
     }
 
 }
+
+
+// funcion que muestra la lista de amigos en el HTML
 
 function mostrarListadoAmigos() {
 
@@ -29,4 +44,21 @@ function mostrarListadoAmigos() {
 
 }
 
+// funcion que genera un numero aleatorio
+function generarNumeroAleatorio() {
+    return Math.floor(Math.random() * arregloDeAmigos.length);
+}
 
+// funcion que muestra el amigo sorteado
+function sortearAmigo() {
+    // validamos si el arregloAmigos contiene valores
+    if (arregloDeAmigos.length !== 0) {
+        amigoSecreto = generarNumeroAleatorio();
+        console.log(amigoSecreto);
+        asignarTextoHTML('#resultado', `El amigo secreto es: ${arregloDeAmigos[amigoSecreto]}`);
+
+    } else {
+        alert('No hay amigos para sortear');
+    }
+
+}
