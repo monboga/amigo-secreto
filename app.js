9f327077cc0dead8632140342814d7fcd818ed62
Gabriel Monroy Garcia <87322582+monboga@users.noreply.github.com>
2025-08-10 21:09:20 -0600

    feat: se implemento validacion del arreglo y validacion de input, junto con el sorteo de amigo (#2)
    * feat: se implementaron nuevas funciones que realizan la adicion de un amigo y mostrarlos por pantalla
    
    * feat: se implemento funcion para sortear y validar cuando no se ingresa algun valor en el campo de texto o arreglo vacio

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// declaramos un arreglo vacio de amigos
+let arregloDeAmigos = [];
+let amigoSecreto = 0;
+
+// funcion para obtener elemento html
+function asignarTextoHTML(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return elementoHTML
+}
+
+
+// inicio de la funcion par agregar un amigo al arreglo.
+function agregarAmigo() {
+    // declaramos la entrada de amigos y obtenemos su valor
+    let campoTextoAmigos = document.querySelector('#amigo');
+    let amigo = campoTextoAmigos.value.trim();
+
+    if (amigo !== '') {
+        arregloDeAmigos.push(amigo);
+        campoTextoAmigos.value = '';
+        mostrarListadoAmigos();
+    } else {
+        alert('Debes ingresar texto en el campo de texto');
+    }
+
+}
+
+
+// funcion que muestra la lista de amigos en el HTML
+
+function mostrarListadoAmigos() {
+
+    let elementoUlHTML = document.querySelector('#listaAmigos');
+    elementoUlHTML.innerHTML = '';
+
+    for (let i = 0; i < arregloDeAmigos.length; i++) {
+        let listaHTML = document.createElement('li');
+        listaHTML.textContent = arregloDeAmigos[i];
+        elementoUlHTML.appendChild(listaHTML);
+
+    }
+
+}
+
+// funcion que genera un numero aleatorio
+function generarNumeroAleatorio() {
+    return Math.floor(Math.random() * arregloDeAmigos.length);
+}
+
+// funcion que muestra el amigo sorteado
+function sortearAmigo() {
+    // validamos si el arregloAmigos contiene valores
+    if (arregloDeAmigos.length !== 0) {
+        amigoSecreto = generarNumeroAleatorio();
+        console.log(amigoSecreto);
+        asignarTextoHTML('#resultado', `El amigo secreto es: ${arregloDeAmigos[amigoSecreto]}`);
+
+    } else {
+        alert('No hay amigos para sortear');
+    }
+
+}
